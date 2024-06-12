190de0d7fb93fccefd85d19d33e9f5742df2f1fd
sydorovay <sydorovay@gmail.com>
2024-06-12 12:55:10 +0200

    add Prettier formater

---
+++ src/components/Product/Product..jsx
@@ -0,0 +1,10 @@
+
+export const Product = () => {
+  return (
+    <>
+      <h2>Tacos</h2>
+			<img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
+			<p>Price: 999 credits</p>
+    </>
+  );
+};
