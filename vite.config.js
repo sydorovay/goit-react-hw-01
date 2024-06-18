d1d5a2cd6d42195d5bc3a72b01ecfd6360a0595a
sydorovay <sydorovay@gmail.com>
2024-06-18 22:26:45 +0200

    change path in app

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     sourcemap: true,
   }
