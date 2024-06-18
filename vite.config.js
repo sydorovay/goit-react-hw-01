122adf934629598fb1e8d99d3a5ab795d8ad6cc1
sydorovay <sydorovay@gmail.com>
2024-06-18 22:14:39 +0200

    update vite json

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
     sourcemap: true,
   }
