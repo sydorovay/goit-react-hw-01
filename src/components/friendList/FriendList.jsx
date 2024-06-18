c2d36a8705af396ebdebaa706a9de46a8ab40d89
sydorovay <sydorovay@gmail.com>
2024-06-18 22:28:46 +0200

    change path

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import FriendListItem from '../FriendListItem/FriendListItem';
-import styles from '../FriendList/FriendList.module.css';
+import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   const { friendlist, friendListItem } = styles;
